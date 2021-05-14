e2779b7792cceadb916f33a6ef3a892d8fd523ff
Robert Booth <boothr16@gmail.com>
2021-05-14 10:18:22 -0400

    Added exercise 1-12.

---
+++ 1-12.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	int c;
+    while ((c = getchar()) != EOF) {
+        if (c != ' ')
+            putchar(c);
+        else {putchar('\n');}
+    }
+	return 0;
+}
